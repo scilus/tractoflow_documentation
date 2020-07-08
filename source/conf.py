aca5224c500174c29764f1079d0a7b0f47b71665
Guillaume Theaud <guillaume.theaud@gmail.com>
2020-07-08 10:03:11 -0400

    Add logo with background

--- source/conf.py
+++ source/conf.py
@@ -89,7 +89,7 @@ html_theme_options = {
     'display_version': True,
 }
 
-html_extra_path = ["data/install.sh"]
+html_extra_path = ["data/install.sh", "data/logo_bg.png"]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
