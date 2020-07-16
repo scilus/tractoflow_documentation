71bb1655e11489ab14d7935bb3a349548ffa309e
Guillaume Theaud <guillaume.theaud@gmail.com>
2020-07-16 10:01:59 -0400

    Update singularity link

--- source/data/install.sh
+++ source/data/install.sh
@@ -6,5 +6,5 @@ unzip -q tractoflow-2.1.1.zip
 rm -rf tractoflow-2.1.1.zip
 echo "Done: TractoFlow downloaded."
 echo "Downloading TractoFlow Singularity..."
-wget -q --show-progress http://scil.usherbrooke.ca/containers_list/tractoflow_2.1.0_5f749f3_2020-06-29.img
+wget -q --show-progress http://scil.dinf.usherbrooke.ca/containers_list/tractoflow_2.1.1_650f776_2020-07-15.img
 echo "Done: TractoFlow Singularity downloaded."
