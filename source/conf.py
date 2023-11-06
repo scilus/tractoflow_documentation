3b5d2a7f494eded096577749d31c290e7214ca72
arnaudbore <arnaud.bore@gmail.com>
2023-11-06 15:24:29 -0500

    fix documentation

--- source/conf.py
+++ source/conf.py
@@ -20,7 +20,7 @@
 # -- Project information -----------------------------------------------------
 
 project = u'TractoFlow-documentation'
-copyright = u'2022, SCIL'
+copyright = u'2023, SCIL'
 author = u'SCIL'
 
 # The short X.Y version
